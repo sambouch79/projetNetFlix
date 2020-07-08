0120e5e2f210dfcda6182699e1f13c5619e93d0e
Samantha D <defeversamantha.pro@gmail.com>
2020-07-08 23:26:35 +0200

    Version 0.0

--- fakenetflix/src/Home.js
+++ fakenetflix/src/Home.js
@@ -1,26 +1,38 @@
 import React, { Component } from "react"
 
 
+
 class Home extends Component {
     constructor(props) {
         super(props)
+        
     }
 
     render(){
         return (
             <div className="p-3 mb-2 bg-dark text-white">
-                <header>
-                    Samira
-                </header>
-
-                <div>  
-                <div><h1>Bandeau Pub</h1>
+              
+                 <div className='row'>  
+                <div><h1 className="text-left">Bandeau Pub</h1></div>
+                <div>  <img src="https://picsum.photos/id/237/1400/300"/>
                 </div>
-                    <div className="container row flex">
-                        <h1>Up Coming </h1>
+                    <div className="row flex">
+                        <h1 className="text-left">Up Coming </h1>
                     </div>
-                    <div className="row">                        
-                        <div className="col-2" >  <img src="https://picsum.photos/id/237/200/300"/></div>
+                    <div></div>
+                    <div className="row flex">                        
+                        <div className="col-2" > <img src="https://picsum.photos/id/237/200/300"/></div>
+                        <div className="col-2"> <img src="https://picsum.photos/id/566/200/300"/> </div> 
+                        <div className="col-2"> <img src="https://picsum.photos/id/568/200/300"/> </div> 
+                        <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
+                        <div className="col-2"> <img src="https://picsum.photos/id/568/200/300"/> </div> 
+                        <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
+                    </div>
+                    <div className="row flex">
+                        <h1 className="text-left">Popular</h1>
+                    </div>
+                    <div className="row flex">
+                    <div className="col-2" >  <img src="https://picsum.photos/id/237/200/300"/></div>
                         <div className="col-2"> <img src="https://picsum.photos/id/566/200/300"/> </div> 
                         <div className="col-2"> <img src="https://picsum.photos/id/568/200/300"/> </div> 
                         <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
@@ -28,7 +40,7 @@ class Home extends Component {
                         <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
                     </div>
                     <div className="row">
-                        <h1>Popular</h1>
+                        <h1 className="text-left">Top Rated</h1>
                     </div>
                     <div className="row">
                     <div className="col-2" >  <img src="https://picsum.photos/id/237/200/300"/></div>
@@ -39,18 +51,7 @@ class Home extends Component {
                         <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
                     </div>
                     <div className="row">
-                        <h1>Top Rated</h1>
-                    </div>
-                    <div className="row">
-                    <div className="col-2" >  <img src="https://picsum.photos/id/237/200/300"/></div>
-                        <div className="col-2"> <img src="https://picsum.photos/id/566/200/300"/> </div> 
-                        <div className="col-2"> <img src="https://picsum.photos/id/568/200/300"/> </div> 
-                        <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
-                        <div className="col-2"> <img src="https://picsum.photos/id/568/200/300"/> </div> 
-                        <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
-                    </div>
-                    <div className="row">
-                        <h1>Now playing</h1>
+                        <h1 className="text-left">Now playing</h1>
                     </div>
                     <div className="row">
                     <div className="col-2" >  <img src="https://picsum.photos/id/237/200/300"/></div>
@@ -61,8 +62,7 @@ class Home extends Component {
                         <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
                     </div>
                 </div>
-                <footer></footer>
-                </div>
+            </div>
         )
     }
 }
