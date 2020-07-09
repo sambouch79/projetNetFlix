82fc00fd37c52f014dc8a89c1128d995984f4150
sambouch79 <62301753+sambouch79@users.noreply.github.com>
2020-07-09 14:36:51 +0200

    correction slider

--- fakenetflix/src/Popular.js
+++ fakenetflix/src/Popular.js
@@ -5,8 +5,18 @@ export default class Popular extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            films: dataservices.films
 
         }
+    }
+    renderImages = () => {
+        let images = []
+        for (let i of this.state.films) {
+            console.log(i.Imgband)
+            images.push(<img src={i.Imgband} />)
+
+        }
+
     }
 
     render() {
@@ -16,12 +26,19 @@ export default class Popular extends Component {
             <div className="container">
                 <h1 className="text-left">Popular</h1>
                 <div className="row flex">
-                    <div className="col-2" > <img src="https://picsum.photos/id/237/200/300" /></div>
-                    <div className="col-2"> <img src="https://picsum.photos/id/566/200/300" /> </div>
-                    <div className="col-2"> <img src="https://picsum.photos/id/568/200/300" /> </div>
-                    <div className="col-2"> <img src="https://picsum.photos/id/237/200/300" /> </div>
-                    <div className="col-2"> <img src="https://picsum.photos/id/568/200/300" /> </div>
-                    <div className="col-2"> <img src="https://picsum.photos/id/237/200/300" /> </div>
+
+                    <div className="row m-1">
+                        <h1 className="col">{this.state.films.title}</h1>
+                    </div>
+                    <div className='row m-1'>
+                        {this.renderImages()}
+                    </div>
+                    <div className='row m-1'>
+                        <div className='col'>
+                            {this.state.films.Overview}
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
