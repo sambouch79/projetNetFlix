f93a6397d6ee1de13883cbe408379dfb6e368db8
E MANTE <mante.depelchin@gmail.com>
2020-07-09 15:30:07 +0200

    version avec upcoming

--- fakenetflix/src/Home.js
+++ fakenetflix/src/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import {Upcoming} from "./upcoming/Upcoming"
 
 
 
@@ -11,23 +12,18 @@ class Home extends Component {
     render(){
         return (
             <div className="p-3 mb-2 bg-dark text-white">
-              
-                 <div className='row'>  
+
+                 
                 <div><h1 className="text-left">Bandeau Pub</h1></div>
                 <div>  <img src="https://picsum.photos/id/237/1400/300"/>
                 </div>
-                    <div className="row flex">
-                        <h1 className="text-left">Up Coming </h1>
+
+                    <div className="">
+                        <h1 className="text-left">  Up Coming </h1>
+                        <Upcoming></Upcoming>
                     </div>
-                    <div></div>
-                    <div className="row flex">                        
-                        <div className="col-2" > <img src="https://picsum.photos/id/237/200/300"/></div>
-                        <div className="col-2"> <img src="https://picsum.photos/id/566/200/300"/> </div> 
-                        <div className="col-2"> <img src="https://picsum.photos/id/568/200/300"/> </div> 
-                        <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
-                        <div className="col-2"> <img src="https://picsum.photos/id/568/200/300"/> </div> 
-                        <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
-                    </div>
+                    
+                    
                     <div className="row flex">
                         <h1 className="text-left">Popular</h1>
                     </div>
@@ -62,7 +58,7 @@ class Home extends Component {
                         <div className="col-2"> <img src="https://picsum.photos/id/237/200/300"/> </div>
                     </div>
                 </div>
-            </div>
+            
         )
     }
 }
